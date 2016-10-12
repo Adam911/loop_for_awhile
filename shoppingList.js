5300e252572f7051e17e1d670a90b5e98b2ad609
Dwayne <saulsdwayne@gmail.com>
2016-10-12 11:37:32 +0200

    shoppingList.js fixed

--- shoppingList.js
+++ shoppingList.js
@@ -6,10 +6,11 @@ var groceries = '1x bread, 6x hotdog rolls, 2x tubs of margarine ';
 function shoppingList(stringToSplit) {
   var arrayOfStrings = stringToSplit.split(',');
 
-  console.log('Shopping list: ')
-  console.log(arrayOfStrings[0]);
-  console.log(arrayOfStrings[1]);
-  console.log(arrayOfStrings[2]);
+  for (var i = 0; i < arrayOfStrings.length; i++) {
+     console.log(arrayOfStrings[i]);
+   }
+
+
 }
 
-shoppingList(groceries);
+shoppingList('Shopping List: '+groceries);
